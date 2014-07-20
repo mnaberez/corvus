47c0af5d59e0db5ee71fb2be62bdb7ef7c9c1d70
Mike Naberezny <mike@naberezny.com>
2014-07-20 14:36:26 -0700

    Mark locations used by the ROM

--- disasm/prep-corvus-diag.asm
+++ disasm/prep-corvus-diag.asm
@@ -97,7 +97,7 @@ format_drive:
     ld de,8200h         ;8052 11 00 82
     ldir                ;8055 ed b0
     ld hl,0000h         ;8057 21 00 00
-    ld (var_2),hl       ;805a 22 fe 81
+    ld (l81fe),hl       ;805a 22 fe 81
     call format         ;805d cd 3d 00
     ld hl,0000h         ;8060 21 00 00
     ld (6012h),hl       ;8063 22 12 60
@@ -110,9 +110,9 @@ read_firm_blk:
 ;1 byte left to read: head/sector
 ;
     call host_read      ;A = read 1 byte from the host
-    ld (var_1),a        ;806c 32 fd 81
+    ld (l81fd),a        ;806c 32 fd 81
     ld hl,0000h         ;806f 21 00 00
-    ld (var_2),hl       ;8072 22 fe 81
+    ld (l81fe),hl       ;8072 22 fe 81
     rst 10h             ;8075 d7
     ld hl,7600h         ;8076 21 00 76
     ld (6012h),hl       ;8079 22 12 60
@@ -127,13 +127,13 @@ writ_firm_blk:
     ld bc,0201h         ;BC = 513 bytes to read
     call host_read_bc   ;Read BC bytes from the host
     ld a,(hl)           ;8085 7e
-    ld (var_1),a        ;8086 32 fd 81
+    ld (l81fd),a        ;8086 32 fd 81
     ld hl,0000h         ;8089 21 00 00
-    ld (var_2),hl       ;808c 22 fe 81
+    ld (l81fe),hl       ;808c 22 fe 81
     rst 20h             ;808f e7
     jp nz,finish_cmd    ;8090 c2 a0 81
     ld hl,0001h         ;8093 21 01 00
-    ld (var_2),hl       ;8096 22 fe 81
+    ld (l81fe),hl       ;8096 22 fe 81
     rst 20h             ;8099 e7
     ld hl,0000h         ;809a 21 00 00
     ld (6012h),hl       ;809d 22 12 60
@@ -149,9 +149,9 @@ verify_drive:
     ld (6012h),hl       ;80a6 22 12 60
     xor a               ;80a9 af
     ld (0a200h),a       ;80aa 32 00 a2
-    ld (var_1),a        ;80ad 32 fd 81
+    ld (l81fd),a        ;80ad 32 fd 81
     ld hl,0000h         ;80b0 21 00 00
-    ld (var_2),hl       ;80b3 22 fe 81
+    ld (l81fe),hl       ;80b3 22 fe 81
 l80b6h:
     call sub_80ddh      ;80b6 cd dd 80
     call sub_810dh      ;80b9 cd 0d 81
@@ -175,18 +175,18 @@ sub_80ddh:
     call 000bh          ;80dd cd 0b 00
     call sub_81a9h      ;80e0 cd a9 81
     ret nz              ;80e3 c0
-    ld a,(var_1)        ;80e4 3a fd 81
+    ld a,(l81fd)        ;80e4 3a fd 81
     and 0e0h            ;80e7 e6 e0
     call sub_813bh      ;80e9 cd 3b 81
-    ld a,(var_1)        ;80ec 3a fd 81
+    ld a,(l81fd)        ;80ec 3a fd 81
     and 0e0h            ;80ef e6 e0
     add a,01h           ;80f1 c6 01
     call sub_813bh      ;80f3 cd 3b 81
-    ld a,(var_1)        ;80f6 3a fd 81
+    ld a,(l81fd)        ;80f6 3a fd 81
     and 0e0h            ;80f9 e6 e0
     add a,02h           ;80fb c6 02
     call sub_813bh      ;80fd cd 3b 81
-    ld a,(var_1)        ;8100 3a fd 81
+    ld a,(l81fd)        ;8100 3a fd 81
     and 0e0h            ;8103 e6 e0
     add a,03h           ;8105 c6 03
     call sub_813bh      ;8107 cd 3b 81
@@ -197,17 +197,17 @@ sub_810dh:
     rrca                ;8111 0f
     rrca                ;8112 0f
     ld b,a              ;8113 47
-    ld a,(var_1)        ;8114 3a fd 81
+    ld a,(l81fd)        ;8114 3a fd 81
     and 0e0h            ;8117 e6 e0
     add a,20h           ;8119 c6 20
-    ld (var_1),a        ;811b 32 fd 81
+    ld (l81fd),a        ;811b 32 fd 81
     cp b                ;811e b8
     jr c,l8138h         ;811f 38 17
     xor a               ;8121 af
-    ld (var_1),a        ;8122 32 fd 81
-    ld hl,(var_2)       ;8125 2a fe 81
+    ld (l81fd),a        ;8122 32 fd 81
+    ld hl,(l81fe)       ;8125 2a fe 81
     inc hl              ;8128 23
-    ld (var_2),hl       ;8129 22 fe 81
+    ld (l81fe),hl       ;8129 22 fe 81
     ex de,hl            ;812c eb
     ld hl,(6002h)       ;812d 2a 02 60
     or a                ;8130 b7
@@ -226,25 +226,25 @@ sub_813bh:
     ld (hl),a           ;8145 77
     ld a,b              ;8146 78
 l8147h:
-    ld (var_1),a        ;8147 32 fd 81
+    ld (l81fd),a        ;8147 32 fd 81
     rst 30h             ;814a f7
     call 0013h          ;814b cd 13 00
     jr nc,l8193h        ;814e 30 43
     rst 10h             ;8150 d7
     ld hl,(6012h)       ;8151 2a 12 60
-    ld a,(var_1)        ;8154 3a fd 81
+    ld a,(l81fd)        ;8154 3a fd 81
     rlc a               ;8157 cb 07
     rlc a               ;8159 cb 07
     rlc a               ;815b cb 07
     and 07h             ;815d e6 07
     ld (hl),a           ;815f 77
     inc hl              ;8160 23
-    ld bc,(var_2)       ;8161 ed 4b fe 81
+    ld bc,(l81fe)       ;8161 ed 4b fe 81
     ld (hl),c           ;8165 71
     inc hl              ;8166 23
     ld (hl),b           ;8167 70
     inc hl              ;8168 23
-    ld a,(var_1)        ;8169 3a fd 81
+    ld a,(l81fd)        ;8169 3a fd 81
     and 1fh             ;816c e6 1f
     ld (hl),a           ;816e 77
     inc hl              ;816f 23
@@ -264,7 +264,7 @@ l8184h:
     rst 18h             ;818f df
     call 00a7h          ;8190 cd a7 00
 l8193h:
-    ld a,(var_1)        ;8193 3a fd 81
+    ld a,(l81fd)        ;8193 3a fd 81
     add a,04h           ;8196 c6 04
     ld hl,60cah         ;8198 21 ca 60
     dec (hl)            ;819b 35
@@ -324,10 +324,21 @@ host_read_bc:
     jp 007dh            ;81ed c3 7d 00
 
 unused:
-    db 0,0,0,0,0,0,0,0,0f8h,1,0,0,2
+    db 0,0,0,0,0,0,0
 
-var_1:
-    db 0
+;The locations below are accessed by the ROM and can't be moved.
 
-var_2:
-    dw 0101h
+l81f7:
+    db 0                ;used by ROM at 046eh
+l81f8:
+    db 0f8h
+l81f9:
+    db 1
+l81fa:
+    dw 0                ;used by ROM at 0c69h
+l81fc:
+    db 2                ;used by ROM at 0847h
+l81fd:
+    db 0                ;used by ROM at 0afbh, 0b68h, 0b81h, ...
+l81fe:
+    dw 0101h            ;used by ROM at 01f4h, 0711h, 0719h, ...
